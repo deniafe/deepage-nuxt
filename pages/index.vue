f8694da5b0d63823abbbb0df9b1dc4043f2c2efe
deniafe <detoyin@yahoo.com>
2021-05-27 12:54:39 +0100

    Started with fcm part 2

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@ export default {
   name: 'Home',
   async fetch(context) {
     const { store, route, req, app, $fire } = context
-    const { commit, dispatch, state } = store
+    const { commit, dispatch } = store
 
     const url = req.headers.host
 
@@ -20,21 +20,21 @@ export default {
     // TODO: THIS WAS SUPPOSED TO BE HOW TO GET THE UNIQUE PAGES. NOW IT'S DIFFERENT. WE ARE USING PARAMS
     dispatch('app/getSubdomain', url)
     // TODO : COMMENT THIS OUT EVERYTIME YOU NEED TO DEPLOY
-    const subDomain = state.subDomain || 'testing'
+    // const subDomain = state.subDomain || 'testing'
 
     // TODO : UNCOMMENT THIS EVERYTIME YOU NEED TO DEPLOY
-    // const getSubdomain = (hostname) => {
-    //   let subDomain
-    //   // eslint-disable-next-line no-useless-escape
-    //   const regexParse = /[a-z\-0-9]{2,63}\.[a-z\.]{2,5}$/
-    //   const urlParts = regexParse.exec(hostname)
-    //   if (urlParts) {
-    //     subDomain = hostname.replace(urlParts[0], '').slice(0, -1)
-    //   }
-    //   return subDomain
-    // }
+    const getSubdomain = (hostname) => {
+      let subDomain
+      // eslint-disable-next-line no-useless-escape
+      const regexParse = /[a-z\-0-9]{2,63}\.[a-z\.]{2,5}$/
+      const urlParts = regexParse.exec(hostname)
+      if (urlParts) {
+        subDomain = hostname.replace(urlParts[0], '').slice(0, -1)
+      }
+      return subDomain
+    }
+    const subDomain = getSubdomain(url)
 
-    // const subDomain = getSubdomain(url)
     console.log('app subDomain', subDomain)
     dispatch('app/getIp')
 
