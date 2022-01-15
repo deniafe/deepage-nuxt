e1be847a38c884185e289cb7065534d4c311967d
deniafe <detoyin@yahoo.com>
2022-01-15 16:21:51 +0100

    Made some slight changes to config files

--- nuxt.config.js
+++ nuxt.config.js
@@ -123,16 +123,5 @@ export default {
   // },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    extend(config, { isDev, isClient, isServer }) {
-      config.resolve.alias.vue = 'vue/dist/vue.common'
-      if (isServer) {
-        config.externals = {
-          '@firebase/app': 'commonjs @firebase/app',
-          '@firebase/firestore': 'commonjs @firebase/firestore',
-          // etc...
-        }
-      }
-    },
-  },
+  build: {},
 }
